3bd655a25f86f548f26d27cdce925ddcf18dfbb2
Chris Billington <chrisjbillington@gmail.com>
2011-03-03 02:57:30 +0000

    timing tests

--- reader.py
+++ reader.py
@@ -1,7 +1,7 @@
 import sys
 import time
 myfile = open(sys.argv[1])
-start_time = time.time
+start_time = time.time()
 x = myfile.read()
 myfile.close()
 print "time taken to read:", time.time() - start_time
