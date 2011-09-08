c0f28c285da5be2b4e3c7fad6c5b31e41fffe006
Chris Billington <chrisjbillington@gmail.com>
2011-09-08 08:55:21 +0000

    pulseblaster.py fixed up -- appears to work now. It takes as command line arguments the initial amplitudes and frequencies of the DDSs of all pulseblasters, and it prints to stdout the final frequencies and amplitudes for use by LabVIEW in maintaining output continuity.
    usege:
    
    python pulseblaster.py dds0initamp dds0initfreq dds1initamp dds1initfreq runfile.h5
    
    This will return:
    
    dds0finalamp dds0finalfreq dds1finalamp dds1finalfreq.
    
    If there is more than one pulesblaster, simply add another four arguments for each one, before the h5 file.
    
    Frequencies in MHz, amplitudes from 0 to 1.

--- deviceprogramming/pulseblaster.py
+++ deviceprogramming/pulseblaster.py
@@ -4,9 +4,9 @@ import h5py
 
 def program_a_dds(number, group, initialamp, initialfreq):
     pb_select_dds(number)
-    amps = group['AMP_REGS']
-    freqs = group['FREQ_REGS']
-    phases = group['PHASE_REGS']
+    amps = group['AMP_REGS'][:] # slice copies, we don't intend to modify the originals
+    freqs = group['FREQ_REGS'][:]
+    phases = group['PHASE_REGS'][:]
     
     amps[0] = initialamp
     freqs[0] = initialfreq
@@ -26,7 +26,6 @@ def program_a_pulseblaster(number, group, initamp0, initfreq0, initamp1, initfre
     for args in group['PULSE_PROGRAM']:
         pb_inst_dds2(*args)
     pb_stop_programming()
-    pb_close()
     #Let's get the final state of the DDS's. z's are the args I
     #don't care about:
     freqreg0,z,ampreg0,z,z,freqreg1,z,ampreg1,z,z,z,z,z,z = args
@@ -34,7 +33,6 @@ def program_a_pulseblaster(number, group, initamp0, initfreq0, initamp1, initfre
     finalfreq1 = freqs1[freqreg1]
     finalamp0 = amps0[ampreg0]
     finalamp1 = amps1[ampreg1]
-    
     return finalamp0, finalfreq0, finalamp1, finalfreq1 
 
 
@@ -50,22 +48,20 @@ if __name__== '__main__':
     assert len(raw_initialstates) % 4 == 0
     for i in range(len(raw_initialstates)/4):
         initial_states[i] = raw_initialstates[4*i:4*(i + 1)]
-    print initial_states
-    assert False
     with h5py.File(sys.argv[-1],'r') as hdf5_file:
         try:
             pb_init()
             pb_core_clock(75)
-            for group in hdf5_file:
+            for group in hdf5_file['devices']:
                 if group.startswith('pulseblaster'):
-                    number = group.split('_')[-1]
-                    final_states[number] = program_a_pulseblaster(number, group, *initialstates[number])
+                    number = int(group.split('_')[-1])
+                    final_states[number] = program_a_pulseblaster(number, hdf5_file['devices/%s'%group], *initial_states[number])
             pb_close()
         except:
             pb_close()
             raise
     for pb in final_states:
-        for item in final_states.items():
+        for item in final_states[pb]:
             print item,
         
 
