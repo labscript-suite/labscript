c2c84cbf0aac41ac643c30be3421cabdff94482c
Chris Billington <chrisjbillington@gmail.com>
2012-07-30 05:52:46 +0000

    Reverted previous change, it had a serious flaw that made the whole thing not work! Phil is implementing correctly.

--- functions.py
+++ functions.py
@@ -1,20 +1,21 @@
 from __future__ import division
 from pylab import *
 
-def ramp(duration, initial, final):
+def ramp(t, duration, initial, final):
     m = (final - initial)/duration
-    return lambda t: m*t + initial
+    c = initial - m*t
+    return lambda x: m*x + c
 
-def sine(duration, amplitude, angfreq, phase, dc_offset):
-    return lambda t: amplitude*sin(angfreq*t + phase) + dc_offset
+def sine(t, duration, amplitude, angfreq, phase, dc_offset):
+    return lambda x: amplitude*sin(angfreq*(x-t) + phase) + dc_offset
     
-def sine_ramp(duration, initial, final):
-    return lambda t: (final-initial)*(sin(pi*t/(2*duration)))**2 + initial
+def sine_ramp(t, duration, initial, final):
+    return lambda x: (final-initial)*(sin(pi*(x-t)/(2*duration)))**2 + initial
     
-def exp_ramp(duration, initial, final, zero):
+def exp_ramp(t,duration,initial,final,zero):
     rate = 1/duration * log((initial-zero)/(final-zero))
-    return lambda t: (initial-zero)*exp(-rate*t) + zero
+    return lambda x: (initial-zero)*exp(-rate*(x-t)) + zero
     
-def exp_ramp_t(duration, initial, final, time_constant):
+def exp_ramp_t(t,duration,initial,final,time_constant):
     zero = (final-initial*exp(-duration/time_constant)) / (1-exp(-duration/time_constant))
-    return lambda t: (initial-zero)*exp(-t/time_constant) + zero
+    return lambda x: (initial-zero)*exp(-(x-t)/time_constant) + zero
