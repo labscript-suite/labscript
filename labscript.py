589cea22da8f94cb0354c2599a0b365325e6f6cb
Chris Billington <chrisjbillington@gmail.com>
2013-08-27 16:12:02 +0200

    Reverted change where a Camera could be triggered by an analog output. This was more trouble than it was worth.

--- labscript.py
+++ labscript.py
@@ -1434,15 +1434,13 @@ class WaitMonitor(Trigger):
         self.timeout_connection = timeout_connection 
         
         
-class Camera(DigitalOut, AnalogOut):
+class Camera(DigitalOut):
     description = 'Generic Camera'
     frame_types = ['atoms','flat','dark','fluoro','clean']
     minimum_recovery_time = 0 # To be set by subclasses
     
-    def __init__(self, name, parent_device, connection, BIAS_port, serial_number, SDK, effective_pixel_size, exposuretime=None, orientation='side', trigger_value=1):
-        Output.__init__(self,name,parent_device,connection)
-        self.trigger_value = trigger_value
-        self.allowed_states = {0:'untriggered', trigger_value:'triggered'}
+    def __init__(self, name, parent_device, connection, BIAS_port, serial_number, SDK, effective_pixel_size, exposuretime=None, orientation='side'):
+        DigitalOut.__init__(self,name,parent_device,connection)
         self.exposuretime = exposuretime
         self.orientation = orientation
         self.exposures = []
@@ -1452,12 +1450,6 @@ class Camera(DigitalOut, AnalogOut):
         self.sn = uint64(serial_number)
         self.sdk = str(SDK)
         self.effective_pixel_size = effective_pixel_size
-        
-    def go_high(self, t):
-        self.add_instruction(t, self.trigger_value)
-    
-    def go_low(self, t):
-        self.add_instruction(t, 0)
         
     def expose(self,name, t , frametype, exposuretime=None):
         self.go_high(t)
