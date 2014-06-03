9cba45c8a5b1f92d5493ee3e6b6d7cd801075f48
Chris Billington <chrisjbillington@gmail.com>
2014-06-03 14:16:05 +1000

    gated-clocks: Tweak to auto initialisation Added command line arg to auto overwrite output file when none is given.

--- __init__.py
+++ __init__.py
@@ -22,9 +22,13 @@ import inspect
 importing_frame = inspect.currentframe()
 importing_locals = importing_frame.f_back.f_locals
 if not 'labscript_auto_init' in importing_locals or importing_locals['labscript_auto_init']:
+    overwrite = False
+    if '-f' in sys.argv:
+        overwrite = True
+        sys.argv.remove('-f')
     if len(sys.argv) > 1:
         labscript_init(sys.argv[1],labscript_file=sys.argv[0])
     elif sys.argv[0]:
-        labscript_init(sys.argv[0].replace('.py','.h5'), labscript_file=sys.argv[0], new=True)
+        labscript_init(sys.argv[0].replace('.py','.h5'), labscript_file=sys.argv[0], new=True, overwrite=overwrite)
         
 __version__ = '1.2.0-gated-clock-development'
