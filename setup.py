67a5a89dae340fce91accc378eb4da0a054cce7f
David Meyer <dihm.meyer@gmail.com>
2024-02-29 16:19:46 -0500

    Remove environment checks in `setup.py` for setuptools_scm

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-import os
 from setuptools import setup
 
 VERSION_SCHEME = {
-    "version_scheme": os.getenv("SCM_VERSION_SCHEME", "release-branch-semver"),
-    "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
+    "version_scheme": "release-branch-semver",
+    "local_scheme": "node-and-date",
 }
 
 setup(use_scm_version=VERSION_SCHEME)
